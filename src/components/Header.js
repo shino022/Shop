2b050281fd71a55cdc4014042a4d802c3d700c52
sss <sshoony95@gmail.com>
2022-08-19 18:29:17 -0400

    Bootstrap header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,28 @@
 import { Link } from "react-router-dom";
-
+import "../styles/Header.css"
 const Header = () => {
   return (
-    <div>
-      <h3>Shop</h3>
-      <nav>
-        <ui>
-          <Link to="/"><li>Home</li></Link> 
-          <Link to="/shopall"><li>Shop All</li></Link>
-          <Link to="/contact"><li>Contact</li></Link>
-        </ui>
-      </nav>
-    </div>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
+        <span className="navbar-brand h1" id="brand">Shop</span>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+          <ui className="navbar-nav mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link" to="/">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/shopall">Shop All</Link>
+            </li>              
+            <li className="nav-item">
+              <Link className="nav-link" to="/contact">Contact</Link>
+            </li>
+          </ui>
+        </div>
+      </div>
+    </nav>
   );
 }
 
