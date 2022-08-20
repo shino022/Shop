09d4694c01fa72372b8273390b249ded6956ea2e
sss <sshoony95@gmail.com>
2022-08-20 18:18:42 -0400

    Add footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,11 @@
+import { FaGithub } from 'react-icons/fa';
+import '../styles/Footer.css';
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <footer className= "d-flex align-item-center bg-dark">
+      <p className="mb-0">Copyright © 2022 shino022</p>
+      <a href="https://github.com/shino022/Shop"><FaGithub /></a>
+    </footer>
   );
 }
 
