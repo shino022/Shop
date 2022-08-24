e3f1accd40641fdca0ae11bd570da4844046337a
sss <sshoony95@gmail.com>
2022-08-23 20:18:35 -0400

    Fix rounting

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
         <p className="mb-5 pb-3 lead">Shop what you love—faster and easier.</p>
         <div className="d-grid pt-5 col-5 mx-auto">
           <button type="button" className="btn btn-outline-dark btn-lg px-4 py-3 fw-bold">
-            <Link className="nav-link" to="/shopall">EXPLORE</Link>
+            <Link className="nav-link" to="/Shop/shopall">EXPLORE</Link>
           </button>
         </div>
       </div>
